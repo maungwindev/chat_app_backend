97cea94b9a60e8767f7bfaca73d60faa08bcae93
maung win <mgwin.developer.2000@gmail.com>
2025-09-12 13:16:53 +0630

    changed:db connection

--- src/models/db.ts
+++ src/models/db.ts
@@ -10,6 +10,21 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
     connectionString: "postgresql://chatdb_wnr3_user:Bg5WD2gayHUX9hBmb1UJvFk6R8VgqqXZ@dpg-d31qhqbuibrs73986jh0-a.singapore-postgres.render.com/chatdb_wnr3",
+    ssl: {
+    rejectUnauthorized: false, // Render/Heroku/Postgres need this
+  },
 })
+
+async function testDbConnection() {
+  try {
+    const client = await pool.connect();
+    console.log("✅ Database connected successfully!");
+    client.release();
+  } catch (err) {
+    console.error("❌ Database connection failed:", err);
+  }
+}
+
+testDbConnection();
 
 export default pool; 
