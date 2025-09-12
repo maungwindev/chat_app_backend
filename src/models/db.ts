6d028cd9c4d9298fb70cd8311d3d4ef60f98220e
maung win <mgwin.developer.2000@gmail.com>
2025-09-12 11:17:37 +0630

    push

--- src/models/db.ts
+++ src/models/db.ts
@@ -1,11 +1,15 @@
 import { Pool } from 'pg';
 
+// const pool = new Pool({
+//     user:'postgres',
+//     password : '1234567890',
+//     host : '127.0.0.1',
+//     port : 5432,
+//     database:"chat_app"
+// });
+
 const pool = new Pool({
-    user:'postgres',
-    password : '1234567890',
-    host : '127.0.0.1',
-    port : 5432,
-    database:"chat_app"
-});
+    connectionString: "postgresql://postgres:SlOIfmCiVGYYyDxpbwuewcguQDHHhUat@tramway.proxy.rlwy.net:59617/railway",
+})
 
 export default pool; 
